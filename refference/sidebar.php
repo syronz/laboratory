229eaf3cb7fee80d15457acd0cf65a4fa91952f8
mosleh.saeidi <mosleh.saeidi@gmail.com>
2020-03-01 23:09:47 +0300

    modify sidebar note

--- refference/sidebar.php
+++ refference/sidebar.php
@@ -14,7 +14,7 @@
 									<h3>About Program</h3>
 
 								<p>This Program designed for limited laboratory actions.</p>
-								<p>For support contact us <br> 0750 514 9171] <br> Email: [sabina.diako@gmail.com] </p>
+								<p>For support contact us at <b> 0750 514 9171 </b> <br> Email: <b>sabina.diako@gmail.com</b> </p>
 							</div>
 						</div>
 					</div>
